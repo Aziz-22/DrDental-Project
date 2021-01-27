434c7c3dee680548b45ec567bad9b950f9045a28
10_Afnan Win_SEI-14 <afnan.s.almajed@gmail.com>
2021-01-27 11:48:17 +0300

    finsh the cards of clinics @Aoismail (#1)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
+import Clinics from './components/Clinics';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> App </h1>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="mainDiv">
+          
+          <Link to="/Clinics">Clinics</Link>
+          <hr />
+          <Route path="/Clinics" component={Clinics} />
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
--- frontend/src/components/Clinics.js
+++ frontend/src/components/Clinics.js
@@ -1,7 +1,122 @@
 import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default class Clinics extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div>
+          <Container>
+            <Row>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      Card Subtitle
+                    </Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                {" "}
+                <Card style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      Card Subtitle
+                    </Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                {" "}
+                <Card style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      Card Subtitle
+                    </Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      Card Subtitle
+                    </Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                {" "}
+                <Card style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      Card Subtitle
+                    </Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                {" "}
+                <Card style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      Card Subtitle
+                    </Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
   }
 }
