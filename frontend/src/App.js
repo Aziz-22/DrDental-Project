406aac983fa36973984d80555f634e23ac91551f
aoismail <abdulelah.o.ismail@gmail.com>
2021-01-27 21:46:35 +0300

    @AfnanSM93 & @aoismail chenges to the following files: App.CSS, App.js, Clinics.js, Progile.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-import Clinics from './components/Clinics';
-import Nav from "./components/Nav";
-
+import Clinics from "./components/Clinics";
+import Profile from "./components/Profile";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <div className="mainDiv">
-          
           <Link to="/Clinics">Clinics</Link>
+          <Link to="/Profile">Profile</Link>
           <hr />
           <Route path="/Clinics" component={Clinics} />
+          <Route path="/Profile" component={Profile} />
         </div>
       </Router>
     );
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,7 +1,102 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
 
 export default class Profile extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div id="profileBox">
+          <Container>
+            <Row>
+              <Col xs={6} md={4}>
+                <Image
+                  src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+                  width="150"
+                  height="150"
+                  roundedCircle
+                />
+                <div class="ImageButton">
+                  <Button variant="outline-dark">Choose your Image</Button>
+                </div>
+                <div class="uplosdImage">
+                  <p>Upload your Image</p>
+                </div>
+              </Col>
+              <Col>
+                <h1>your Profile</h1>
+              </Col>
+            </Row>
+          </Container>
+          <Container>
+            <Container>
+              <Form>
+                <Row>
+                  <Col sm={8}>
+                    {" "}
+                    <Form.Group controlId="formGroupName">
+                      <Form.Label>Edit your Name</Form.Label>
+                      <Form.Control type="Name" placeholder="Enter Name" />
+                    </Form.Group>
+                  </Col>
+                  <Col sm={4}>
+                    {" "}
+                    <p>
+                      <a href="">Update</a>
+                    </p>
+                  </Col>
+                </Row>
+              </Form>
+            </Container>
+            <Container>
+              <Form>
+                <Row>
+                  <Col sm={8}>
+                    {" "}
+                    <Form.Group controlId="formGroupEmail">
+                      <div class="proflabel">
+                        <Form.Label>Edit your Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </div>
+                    </Form.Group>
+                  </Col>
+                  <Col sm={4}>
+                    {" "}
+                    <p>
+                      <a href="">Update</a>
+                    </p>
+                  </Col>
+                </Row>
+              </Form>
+            </Container>
+            <Container>
+              <Form>
+                <Row>
+                  <Col sm={8}>
+                    {" "}
+                    <Form.Group controlId="formGroupPassword">
+                      <Form.Label>Edit your Password</Form.Label>
+                      <div class="proflabel">
+                        <Form.Control type="password" placeholder="Password" />
+                      </div>
+                    </Form.Group>
+                  </Col>
+                  <Col sm={4}>
+                    {" "}
+                    <p>
+                      <a href="">Update</a>
+                    </p>
+                  </Col>
+                </Row>
+              </Form>
+            </Container>
+          </Container>
+        </div>
+      </div>
+    );
   }
 }
