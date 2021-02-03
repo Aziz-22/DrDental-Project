6915b2bd5824cc627bcaf294788ae05e2dcb7717
18_shahad Win_SEI-14 <Shahad.m.alsayed@hotmail.com>
2021-02-03 15:53:07 +0300

    admin can add a new users (#18)

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { getAllClinics, AddNewClinic, deleteClinicByID } from "../api";
+import { getAllClinics, AddNewClinic, deleteClinicByID, userSignUp } from "../api";
 import OneClinicAdmin from "./oneClinicAdmin";
 import $ from 'jquery';
 
@@ -57,9 +57,9 @@ export default class Admin extends React.Component {
 
           console.log(newClinicsList.push(newClinic));
           this.setState({ clinics: newClinicsList });
-          window.$('#exampleModal').modal('toggle'); 
+          window.$('#exampleModal').modal('toggle');
           $('#exampleModal').find("input,select").val('').end();
-          
+
         })
 
         .catch((err) => {
@@ -86,6 +86,22 @@ export default class Admin extends React.Component {
       input["clinicImage"] =
         "https://e7.pngegg.com/pngimages/574/699/png-clipart-medicine-pharmacy-consultant-pharmacist-clinic-icon-scanner-heart-cartoon-thumbnail.png";
     }
+    if (!input["firstName"]) {
+      isValid = false;
+      errors["firstName"] = "Please enter your first Name.";
+    }
+    if (!input["lastName"]) {
+      isValid = false;
+      errors["lastName"] = "Please enter your last Name.";
+    }
+    if (!input["email"]) {
+      isValid = false;
+      errors["email"] = "Please enter your email.";
+    }
+    if (!input["password"]) {
+      isValid = false;
+      errors["password"] = "Please enter your password.";
+    }
     this.setState({
       errors: errors,
     });
@@ -109,7 +125,30 @@ export default class Admin extends React.Component {
         console.log("ERR: ", err);
       });
   };
+  adduser = (event) => {
+    event.preventDefault();
+    if(this.validate()){
+     const userinfo = this.state.input
+     userSignUp(userinfo)
+      .then((res) => {
+        console.log("res");
+        console.log(res);
+        console.log(res.data);
+        alert("User created successfully.");
 
+        window.$('#exampleModalCenter').modal('toggle');
+        $('#exampleModalCenter').find("input,select").val('').end();
+
+
+      })
+
+
+      .catch((err) => {
+        console.log("ERR: ", err);
+      });
+    }
+
+  }
   render() {
     const allClinics = this.state.clinics.map((clinic, index) => {
       return (
@@ -130,7 +169,7 @@ export default class Admin extends React.Component {
     });
     return (
       <div>
-        <br/>
+        <br />
         {/* <!-- Button trigger modal for adding new clinic --> */}
         <div class="col-md-12 text-center">
           <button
@@ -141,22 +180,26 @@ export default class Admin extends React.Component {
           >
             Add New Clinic
           </button>
+          <br/>
+          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModalCenter">
+            Add New User
+          </button>
         </div>
         <hr></hr>
         <div className="mr-4">
-        <table className="table ml-4">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Services</th>
-              <th scope="col">Rating</th>
-              <th scope="col">Image</th>
-              <th scope="col">Edit/Delete</th>
-            </tr>
-          </thead>
-          {allClinics}
-        </table>
+          <table className="table ml-4">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Services</th>
+                <th scope="col">Rating</th>
+                <th scope="col">Image</th>
+                <th scope="col">Edit/Delete</th>
+              </tr>
+            </thead>
+            {allClinics}
+          </table>
         </div>
         {/* <!-- Modal for adding new clinic --> */}
         <div
@@ -266,6 +309,91 @@ export default class Admin extends React.Component {
                   </form>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Add A New User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form onSubmit={this.adduser}>
+                  <div class="col-md-12">
+                    <label for="InputFirstName">First Name</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="InputFirstName"
+                      aria-describedby="firstName"
+                      placeholder="First Name"
+                      name="firstName"
+                      onChange={this.handleChange}
+                    />
+                    <div className="text-danger">
+                        {this.state.errors.firstName}
+                      </div>
+                    <label for="InputLastName">Last Name</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="InputLastName"
+                      aria-describedby="lastName"
+                      placeholder="Last Name"
+                      name="lastName"
+                      onChange={this.handleChange}
+                    />
+                    <div className="text-danger">
+                        {this.state.errors.lastName}
+                      </div>
+
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter email"
+                      name="email"
+                      onChange={this.handleChange}
+                    />
+                    <div className="text-danger">
+                        {this.state.errors.email}
+                      </div>
+                    <label for="Phone">Phone Number</label>
+                    <input
+                      type="number"
+                      class="form-control"
+                      id="Phone"
+                      aria-describedby="Phone Number"
+                      placeholder="Enter Phone Number"
+                      name="phone"
+                      onChange={this.handleChange}
+                    />
+                    <label for="exampleInputPassword1">Password</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="exampleInputPassword1"
+                      placeholder="Password"
+                      name="password"
+                      onChange={this.handleChange}
+                    />
+                    <div className="text-danger">
+                        {this.state.errors.password}
+                      </div>
+                  </div>
+                  <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-info" >Create A New User</button>
+              </div>
+                </form>
+              </div>
+              
             </div>
           </div>
         </div>
